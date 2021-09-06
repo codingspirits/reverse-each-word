07a3f30c8618d15158b3b87413755859db3aef32
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-06 17:02:09 +1000

    Completed Assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,22 @@
+=begin 
 def reverse_each_word_bad sentence
     array = sentence.split('')
     new_array = []
     array.each {|letter| new_array.push(letter)}
     p new_array[3..1]
 end
+=end
 
 def reverse_each_word sentence
-    sentence.split.collect {|word| word.reverse}.join(" ")
+    #sentence.split.collect {|word| word.reverse}.join(" ")
+    new_array = []
+    separated = sentence.split('')
+    separated.each {|letter| new_array.push(letter)}
+    final_array = new_array.reverse
+    final_array.join('')
 end
 
 p reverse_each_word "Hi, I like pie"
+#puts reverse_each_word "Hi, I like pie"
+
 
